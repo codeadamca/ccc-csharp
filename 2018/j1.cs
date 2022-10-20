0f6b8b9c6da9d51556c910bda7aeada4fa1799fc
Adam <thomasadam83@hotmail.com>
2022-10-20 16:24:08 -0400

    Added solution for 2018 J1

---
+++ 2018/j1.cs
@@ -0,0 +1,25 @@
+﻿// Display instructions
+Console.WriteLine("Enter four numbers, press ENTER after each number:");
+
+// Create a string variable and get user input from the keyboard and store it in the variable
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+int num4 = Convert.ToInt32(Console.ReadLine());
+
+// Display the complete series of numbers
+Console.WriteLine("You entered " + num1 + num2 + num3 + num4);
+
+// Check if the number is a telemarketer
+if (num1 > 7 && num2 == num3 && num4 > 7)
+{
+    Console.WriteLine("Ignore");
+}
+else
+{
+    Console.WriteLine("Answer");
+}
+
+// Program complete
+Console.WriteLine("Program complete. Press ENTER to close.");
+Console.ReadKey();
